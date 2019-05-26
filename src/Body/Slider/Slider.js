953ba7c58170ebf4abc748b1b4461388bccec109
Eitan Peer <eitan.peer@cloudinary.com>
2019-05-26 17:06:36 +0300

    interactive sliders

--- src/Body/Slider/Slider.js
+++ src/Body/Slider/Slider.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import ReactInputRange from 'react-input-range';
+import debounce from 'lodash.debounce';
 import 'react-input-range/lib/css/index.css';
 import './Slider.css';
 
@@ -39,8 +40,49 @@ const classNames = {
 const minValue = 0;
 const maxValue = 100;
 
+let activeHandle;
+
+const createImgUrl = (pct) => `https://res.cloudinary.com/demo/video/upload/w_50,so_${pct}p/dog.jpg`
+
+const onChange = debounce((min, max) => {
+    console.log({min, max})
+    const img = activeHandle.firstChild;
+    const isMin = img.id === 'slider-thumb-0';
+    const pct = isMin ? min : max;
+    img.src = createImgUrl(pct);
+}, 150)
+
+const eventHandler = e => {
+    activeHandle = e.target;
+}
+
 function RangeSlider(props) {
     const [range, setRange] = useState({ min: minValue, max: maxValue });
+
+    useEffect(() => {
+        // Use DOM APIs to bind to slider events and add elements to them
+        // Since it is not supported by the input-range component
+        const sliderHandles = [...document.getElementsByClassName('input-range__slider')];
+        sliderHandles.forEach((handle, index) => {
+            // Append a child to the event target if it doesn't have children
+            if (!handle.hasChildNodes()) {
+                const img = document.createElement('img');
+                img.id = `slider-thumb-${index}`;
+                // Offset in %
+                img.src = createImgUrl(index*100);
+                img.style.cssText = "position: absolute; top: -30px; left: -100%"
+                handle.appendChild(img);
+            }
+
+            // Add event handler to denote the active slider
+            handle.addEventListener("mousedown", eventHandler);
+        });
+        return function cleanup() {
+            sliderHandles.forEach(handle => {
+                handle.removeEventListener("mousedown", eventHandler);
+            });
+            }
+      });
 
     return (
         <div>
@@ -49,12 +91,12 @@ function RangeSlider(props) {
                 maxValue={maxValue}
                 minValue={minValue}
                 value={{ min: range.min, max: range.max }}
-                // classNames={classNames}
-                // formatLabel={() => ''}
                 onChange={value => {
                     const min = Math.max(value.min, minValue);
                     const max = Math.min(value.max, maxValue);
+                    // Update slider position
                     setRange({ min, max });
+                    onChange(min, max)
                 }}
             />
         </div>
