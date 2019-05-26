ce16e5a8d105e5aa0d9c3328dd7c1559742628ce
nadavofi <nadav.ofir@cloudinary.com>
2019-05-26 17:39:31 +0300

    docs

---
+++ docs/precache-manifest.e5e3eba4d59021f449a345a80264a42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6cc7a472aec570de4298bdeffe95ba",
+    "url": "/byoce/index.html"
+  },
+  {
+    "revision": "c0fad34ca75b06f87655",
+    "url": "/byoce/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "269b22fb156dcce6659c",
+    "url": "/byoce/static/css/main.228c1cf4.chunk.css"
+  },
+  {
+    "revision": "c0fad34ca75b06f87655",
+    "url": "/byoce/static/js/2.0a8edd17.chunk.js"
+  },
+  {
+    "revision": "269b22fb156dcce6659c",
+    "url": "/byoce/static/js/main.470544a8.chunk.js"
+  },
+  {
+    "revision": "2d184d0074d4957773af",
+    "url": "/byoce/static/js/runtime~main.b38f02e8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50df9f33cf774f71bb1e142be9dba176.js"
+  "/byoce/precache-manifest.e5e3eba4d59021f449a345a80264a42c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/byoce/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
